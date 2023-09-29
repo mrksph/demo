823baef7c58524dbc32ffbbf3c3f5ca1ec99d223
Marcos <markcosta15@hotmail.com>
2023-09-29 18:34:32 +0200

    Add a new endpoint and tests for it

--- src/main/java/com/mrksph/demo/Controller.java
+++ src/main/java/com/mrksph/demo/Controller.java
@@ -1,6 +1,7 @@
 package com.mrksph.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class Controller{
     @GetMapping(value = "/greeting")
     public String greeting() {
         return "Hello World!";
+    }
+
+    @GetMapping(value = "/greeting/{username}")
+    public String greeting(@PathVariable String username) {
+        return String.format("Hello %s!", username);
     }
 }
